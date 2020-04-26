1583afbefe589b90e8ee5f0425ecfc82389c61c4
qlivez <clive.cs06@nctu.edu.tw>
2020-04-26 14:55:36 +0800

    epsilon greedy

--- policy.h
+++ policy.h
@@ -1,6 +1,9 @@
 #include "MonteCarlo.h"
 #include "NN.h"
 #include <string.h>
+#include <stdlib.h>
+#include <time.h>
+
 
 class Policy {
 public:
@@ -52,38 +55,63 @@ public:
 	}
 
 	static Pair NN (board &before, const PIECE &piece) {
-		board now = before;
-
-		// get legal action
-		auto moves = now.get_available_move(piece);
-
-		double max_val = -2.0;
+		// 10% epsilon
+		const int epsilon = 10;
 		Pair best_move{};
 
 		std::cerr << "Net take action: \n\n";
 
-		// enumerate all moves
-		for (auto &mv : moves) {
-			board next = now;
-			next.move(mv.prev, mv.next, piece);
+		std::random_device rd;
+		std::default_random_engine engine(rd());
+		std::uniform_int_distribution<int> distribution(0,100);
 
-			const int stacks = 3; // black*1 + white*1 + take_turn
+		int prob = distribution(engine);
 
-			float tensor_stack[stacks * board::SIZE];
-			generate_states(tensor_stack, next, piece);
-
-			torch::Tensor boards = torch::from_blob(tensor_stack, {1, 3, 6, 6}).to(device); // shape: [batch_size, stacks, row, col]
-			torch::Tensor pred_val = Net->forward(boards).to(device);
-
-			double pred = pred_val[0].item<double>();
+		if ( prob > epsilon ) {	// 1-epsilon : NN output
 			
-			// find the best V(s)
-			if (pred > max_val) {
-				max_val = pred;
-				best_move = mv;				
+			//std::cerr << "exploit: " << prob << '\n';
+
+			board now = before;
+			// get legal action
+			auto moves = now.get_available_move(piece);
+
+			double max_val = -2.0;
+			
+			// enumerate all moves
+			for (auto &mv : moves) {
+				board next = now;
+				next.move(mv.prev, mv.next, piece);
+
+				const int stacks = 3; // black*1 + white*1 + take_turn
+
+				float tensor_stack[stacks * board::SIZE];
+				generate_states(tensor_stack, next, piece);
+
+				torch::Tensor boards = torch::from_blob(tensor_stack, {1, 3, 6, 6}).to(device); // shape: [batch_size, stacks, row, col]
+				torch::Tensor pred_val = Net->forward(boards).to(device);
+
+				double pred = pred_val[0].item<double>();
+				
+				// find the best V(s)
+				if (pred > max_val) {
+					max_val = pred;
+					best_move = mv;				
+				}
 			}
 		}
-		
+		else { // epsilon : random move
+			// std::cerr << "explore: " << prob << '\n';;
+			std::vector<Pair> ea = before.find_piece(piece, EAT), mv = before.find_piece(piece, MOVE), pos;
+			pos.reserve(ea.size() + mv.size());
+			pos.insert(pos.end(), ea.begin(), ea.end());
+			pos.insert(pos.end(), mv.begin(), mv.end());
+
+			if ( !pos.empty() ) {
+				std::shuffle(pos.begin(), pos.end(), engine);
+				best_move = pos[0];
+			}
+		}
+
 		return best_move;		
 	}
 };
